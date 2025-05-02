11f4e27f238019d1fa0f4e69b41c82c1f46c7132
Ilia Egorov <AleilMessia@mail.ru>
2025-05-02 23:08:47 +0300

    added epicTaskId field

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -1,12 +1,22 @@
 package tasks;
 
 public final class SubTask extends BaseTask {
+    Integer epicTaskId;
 
     public SubTask(String title,
                    String description,
                    Integer id,
-                   TaskStatus status) {
+                   TaskStatus status,
+                   Integer epicTaskId) {
         super(title, description, id, status);
+        this.epicTaskId = epicTaskId;
     }
 
+    public Integer getEpicTaskId() {
+        return epicTaskId;
+    }
+
+    public void setEpicTaskId(Integer epicTaskId) {
+        this.epicTaskId = epicTaskId;
+    }
 }
