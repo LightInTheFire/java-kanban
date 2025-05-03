b9ca628d08a34fdfeeaadd2addf69bfb4775f9a5
Ilia Egorov <AleilMessia@mail.ru>
2025-05-03 20:46:34 +0300

    renamed variable get all subtasks of epic

--- src/model/MenuOption.java
+++ src/model/MenuOption.java
@@ -4,7 +4,7 @@ public enum MenuOption {
     GET_ALL_TASKS("Получить все задачи"),
     GET_ALL_TASKS_BY_TYPE("Получить все задачи определенного типа"),
     GET_TASK_BY_ID("Получить задачу по её id"),
-    GET_ALL_SUBTASKS_OF_EPIC("Получить все подзадачи эпика"),
+    GET_ALL_SUBTASKS_OF_EPIC_BY_ID("Получить все подзадачи эпика по его id"),
     DELETE_ALL_TASKS_BY_TYPE("Удалить все задачи определенного типа"),
     CREATE_NEW_TASK("Создать новую задачу"),
     UPDATE_TASK("Обновить задачу"),
