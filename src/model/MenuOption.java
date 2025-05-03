48daa96ba9bc97934e250d075a90ce59d85aeb89
Ilia Egorov <AleilMessia@mail.ru>
2025-05-03 20:18:48 +0300

    MenuOption enum for ConsoleTaskManager

---
+++ src/model/MenuOption.java
@@ -0,0 +1,27 @@
+package model;
+
+public enum MenuOption {
+    GET_ALL_TASKS("Получить все задачи"),
+    GET_ALL_TASKS_BY_TYPE("Получить все задачи определенного типа"),
+    GET_TASK_BY_ID("Получить задачу по её id"),
+    GET_ALL_SUBTASKS_OF_EPIC("Получить все подзадачи эпика"),
+    DELETE_ALL_TASKS_BY_TYPE("Удалить все задачи определенного типа"),
+    CREATE_NEW_TASK("Создать новую задачу"),
+    UPDATE_TASK("Обновить задачу"),
+    DELETE_TASK_BY_ID("Удалить задачу по её id"),
+    EXIT_APP("Выйти из приложения");
+
+    private final String description;
+
+    MenuOption(String description) {
+        this.description = description;
+    }
+
+    public static MenuOption parseValue(int value) {
+        return MenuOption.values()[value - 1];
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
