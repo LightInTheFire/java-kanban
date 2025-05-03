0f9eb46c36310b87b0ae3ec679a803480fc61c82
Ilia Egorov <AleilMessia@mail.ru>
2025-05-03 20:05:17 +0300

    ConsoleTaskManager usage in main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import model.ConsoleTaskManager;
+import model.TaskManager;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        TaskManager taskManager = new TaskManager();
+        Scanner scanner = new Scanner(System.in);
+        ConsoleTaskManager consoleTaskManager = new ConsoleTaskManager(taskManager, scanner);
+        consoleTaskManager.start();
     }
 }
