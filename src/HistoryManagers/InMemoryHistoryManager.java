be4e8f60627d41e287b62a61bbb382a9ecec448f
Ilia Egorov <AleilMessia@mail.ru>
2025-05-17 17:55:54 +0300

    Added InMemoryHistoryManager implementation with list as internal storage

---
+++ src/HistoryManagers/InMemoryHistoryManager.java
@@ -0,0 +1,32 @@
+package HistoryManagers;
+
+import tasks.BaseTask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryHistoryManager implements HistoryManager {
+    private static final int MAX_CAPACITY = 10;
+
+    private final List<BaseTask> history;
+
+    public InMemoryHistoryManager() {
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public void add(BaseTask task) {
+        if (history.size() <= MAX_CAPACITY) {
+            history.addLast(task);
+            return;
+        }
+
+        history.removeFirst();
+        history.addLast(task);
+    }
+
+    @Override
+    public List<BaseTask> getHistory() {
+        return history;
+    }
+}
