b2c2113747cc5ac5044361c2b2953ed99b7d0ae5
Ilia Egorov <AleilMessia@mail.ru>
2025-07-05 12:39:03 +0300

    added tests for fromCSVString method

--- test/ru/light/utils/CSVTaskFormatterTest.java
+++ test/ru/light/utils/CSVTaskFormatterTest.java
@@ -1,12 +1,10 @@
 package ru.light.utils;
 
 import org.junit.jupiter.api.Test;
-import ru.light.task.EpicTask;
-import ru.light.task.SubTask;
-import ru.light.task.Task;
-import ru.light.task.TaskStatus;
+import ru.light.task.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CSVTaskFormatterTest {
 
@@ -14,7 +12,7 @@ class CSVTaskFormatterTest {
     void taskToCSVString() {
         Task task = new Task("Title", "Task Description", 3, TaskStatus.NEW);
         String csvString = CSVTaskFormatter.toCSVString(task);
-        assertEquals("3,TASK,Title,NEW,Task Description,", csvString);
+        assertEquals("3,TASK,Title,NEW,Task Description, ", csvString);
     }
 
     @Test
@@ -28,10 +26,43 @@ class CSVTaskFormatterTest {
     void epicTaskToCSVString() {
         EpicTask task = new EpicTask("Title", "Epic Task Description", 3);
         String csvString = CSVTaskFormatter.toCSVString(task);
-        assertEquals("3,EPICTASK,Title,NEW,Epic Task Description,", csvString);
+        assertEquals("3,EPICTASK,Title,NEW,Epic Task Description, ", csvString);
     }
 
     @Test
-    void fromCSVString() {
+    void taskFromCSVString() {
+        Task task = new Task("Title", "Task Description", 3, TaskStatus.NEW);
+        String csvString = CSVTaskFormatter.toCSVString(task);
+        BaseTask taskFromCsv = CSVTaskFormatter.fromCSVString(csvString);
+        assertEquals(task.getTitle(), taskFromCsv.getTitle());
+        assertEquals(task.getDescription(), taskFromCsv.getDescription());
+        assertEquals(task.getStatus(), taskFromCsv.getStatus());
+        assertEquals(task.getId(), taskFromCsv.getId());
+    }
+
+    @Test
+    void subTaskFromCSVString() {
+        SubTask task = new SubTask("Title", "Task Description", 3, TaskStatus.NEW, 11);
+        String csvString = CSVTaskFormatter.toCSVString(task);
+        SubTask taskFromCsv = (SubTask) CSVTaskFormatter.fromCSVString(csvString);
+        assertEquals(task.getTitle(), taskFromCsv.getTitle());
+        assertEquals(task.getDescription(), taskFromCsv.getDescription());
+        assertEquals(task.getStatus(), taskFromCsv.getStatus());
+        assertEquals(task.getId(), taskFromCsv.getId());
+        assertEquals(task.getEpicTaskId(), taskFromCsv.getEpicTaskId());
+    }
+
+    @Test
+    void EpicTaskFromCSVString() {
+        EpicTask task = new EpicTask("Title", "Epic Task Description", 3);
+        String csvString = CSVTaskFormatter.toCSVString(task);
+        BaseTask taskFromCsv = CSVTaskFormatter.fromCSVString(csvString);
+        assertEquals(task.getTitle(), taskFromCsv.getTitle());
+        assertEquals(task.getDescription(), taskFromCsv.getDescription());
+        assertEquals(task.getStatus(), taskFromCsv.getStatus());
+        assertEquals(task.getId(), taskFromCsv.getId());
+        assertDoesNotThrow(() -> {
+            EpicTask epic = (EpicTask) taskFromCsv;
+        });
     }
 }
