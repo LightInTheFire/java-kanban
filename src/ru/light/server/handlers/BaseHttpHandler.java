1b58873e0e6075164720a2546928fd447dd951bf
Ilia Egorov <AleilMessia@mail.ru>
2025-08-01 03:15:06 +0300

    feat: BaseHttpHandler impl for basic request/response handling

---
+++ src/ru/light/server/handlers/BaseHttpHandler.java
@@ -0,0 +1,66 @@
+package ru.light.server.handlers;
+
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import ru.light.managers.task.TaskManager;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public abstract class BaseHttpHandler implements HttpHandler {
+    public static final Charset STANDARD_CHARSET = StandardCharsets.UTF_8;
+    public static final Map.Entry<String, String> CONTENT_HEADER =
+            Map.entry("Content-Type", "application/json;charset=utf-8");
+    protected final TaskManager taskManager;
+    protected final Gson gson;
+
+    public BaseHttpHandler(TaskManager taskManager, Gson gson) {
+        this.taskManager = taskManager;
+        this.gson = gson;
+    }
+
+    protected void sendResponse(HttpExchange exchange, String response, int statusCode) {
+        byte[] responseInBytes = response.getBytes(STANDARD_CHARSET);
+        exchange.getResponseHeaders().add(CONTENT_HEADER.getKey(), CONTENT_HEADER.getValue());
+        try (OutputStream outputStream = exchange.getResponseBody()) {
+            exchange.sendResponseHeaders(statusCode, responseInBytes.length);
+            outputStream.write(responseInBytes);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to send response", e);
+        }
+    }
+
+    protected void sendResponseNotFound(HttpExchange exchange, String response) {
+        int statusCode = 404;
+        sendResponse(exchange, response, statusCode);
+    }
+
+    protected void sendResponseBadRequest(HttpExchange exchange) {
+        int statusCode = 400;
+        String response = "Unsupported method";
+        sendResponse(exchange, response, statusCode);
+    }
+
+    protected void sendResponseHasOverlaps(HttpExchange exchange, String response) {
+        int statusCode = 406;
+        sendResponse(exchange, response, statusCode);
+    }
+
+    protected String getStringFromRequestBody(HttpExchange exchange) {
+        try (InputStream inputStream = exchange.getRequestBody()) {
+            return new String(inputStream.readAllBytes(), STANDARD_CHARSET);
+        } catch (IOException e) {
+            throw new RuntimeException("Error while reading request body", e);
+        }
+    }
+
+    protected String[] splitURI(HttpExchange exchange) {
+        return exchange.getRequestURI().getPath().split("/");
+    }
+
+}
